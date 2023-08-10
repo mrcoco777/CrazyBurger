c2677301b73ad38b35967507956b97367845b228
mrcoco777 <anguyen77680@gmail.com>
2023-08-09 20:36:47 -0700

    🔥 F01: Delete App.test.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  //state 
+  const [prenom, setPrenom] = useState("Antoine")
+// je definis un state, j'initialiste VI, que je recu variate prenom
+//et je peux que modif avec setPrenom
+  const [count, setCount] = useState(1)
+  //why not use let, because useState provote une reactualisation auto
+  //comportement
+  const handleClick =  () => {
 
+    setCount(count + 1)
+
+  }
+  //affichage (render = rendre via return 
+  //JSX= ecrice html dans JS)
+  //Modifier le state sans utiliser son setter dédié modifie la valeur du state 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hi Antoine</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <h1>Bienvenue chez nous !</h1>
+    <p>Connectez-vous</p>
+    <form>
+       <input type="text" placeholder="Entre votre prenom" required></input>
+       <button onClick={handleClick}>Acceder a votre espace</button>
+       </form>
+    <p>{count}</p>
+    
+  </div>
   )
 }
+
 
 export default App
