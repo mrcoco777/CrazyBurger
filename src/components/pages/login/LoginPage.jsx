502768ce9c2aa902e531244cad4672ffefbb19d0
mrcoco777 <anguyen77680@gmail.com>
2024-01-01 17:16:10 -0800

    ♻️ F01: LOGIN > Create Login Forn

---
+++ src/components/pages/login/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import LoginForm from './LoginForm'
+
+
+export default function LoginPage() {
+  
+
+  
+  return (
+    <LoginForm />
+  )
+     
+
+}
+
